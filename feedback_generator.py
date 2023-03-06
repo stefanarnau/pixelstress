1478139cbb21672bbc3cef031f748308071edf93
plkn <fischmechanik@gmail.com>
2023-03-06 17:02:05 +0100

    nothing really...

---
+++ feedback_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 16:27:35 2023
+
+@author: plkn
+"""
+
+# Imports
+import numpy as np
+
+# Set number of feedbacks (number of sequences in block)
+n_feedbacks = 30
+
+# Iterate feedbacks
+for fb in range(n_feedbacks):
+    print(fb)
