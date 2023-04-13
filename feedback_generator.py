c4c4114c57d829caaa1838844cfe6258a5ba3062
plkn <fischmechanik@gmail.com>
2023-04-13 15:54:44 +0200

    control file generator complete

--- feedback_generator.py
+++ feedback_generator.py
@@ -9,65 +9,197 @@ Created on Mon Mar  6 16:27:35 2023
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+# Path out
+path_out = "/home/plkn/repos/pixelstress/control_files/"
 
 # Number of ids to create files for
 ids = range(10)
 
 # Iterate participants
 for subject_id in ids:
-    
+
+    # Collector for all the numbers
+    all_the_lines = []
+
     # Define block condition pattern (fixed for now)
     outcomes = ["good", "bad", "good", "bad", "good", "bad", "good", "bad"]
     the_path = ["close", "close", "easy", "easy", "close", "close", "easy", "easy"]
-    
+
     # Iterate blocks
     for block_nr in range(8):
-        
+
         # Get relevant outcome factor
-        outcome_factor = {"good":1,"bad":-1}[outcomes[block_nr]]
-        
+        outcome_factor = {"good": 1, "bad": -1}[outcomes[block_nr]]
+
         # Set final value
-        end_point = {"easy": np.random.uniform(0.8, 1.3, (1,)),
-                     "close":np.random.uniform(0.05, 0.15, (1,))}[the_path[block_nr]]
-        
+        end_point = {
+            "easy": np.random.uniform(0.8, 1, (1,)),
+            "close": np.random.uniform(0.05, 0.2, (1,)),
+        }[the_path[block_nr]] * outcome_factor
+
+        if the_path[block_nr] == "easy":
+            outcome_wiggleroom = 1
+        else:
+            outcome_wiggleroom = 0
+
+        # Line for blockstart
+        all_the_lines.append(
+            np.array(
+                [
+                    subject_id + 1,
+                    1,
+                    block_nr + 1,
+                    outcome_factor,
+                    outcome_wiggleroom,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                ]
+            )
+        )
+
         # Get performance scores
-        seq_scores = np.random.uniform(-1, 1, (30,)) * outcome_factor
-        
+        seq_scores = np.random.uniform(-1, 1, (30, 1))
+
         # get non-scaled feedback scores
         feedbacks_non_scaled = np.linspace(0, end_point, 30) + seq_scores
-        
+
         # get scaled version of feedback scores (accumulated)
-        feedbacks = np.linspace(0, end_point, 30) + np.multiply(seq_scores, np.linspace(0.9, 0, 30))
-        
+        feedbacks_scaled = np.linspace(0, end_point, 30) + np.multiply(
+            seq_scores, np.linspace(0.9, 0, 30).reshape(-1, 1)
+        )
+
         # Set outcome
-        feedbacks[-1] = end_point * outcome_factor
-        feedbacks_non_scaled[-1] = end_point * outcome_factor
-        
+        feedbacks_scaled[-1] = end_point
+        feedbacks_non_scaled[-1] = end_point
+
         # Adjust last performance score to match the fixed outcome
         seq_scores[-1] = feedbacks_non_scaled[-1] - feedbacks_non_scaled[-2]
-        
+
         # Get average pixel proportions for sequences
-        pixel_proportions = np.linspace(0.5, 0.25, 30)
-        
+        pixel_proportions = np.linspace(0.49, 0.25, 30)
+
         # Sort pixel proportions by performance scores
-        sort_idx = seq_scores.argsort()
-        
+        sort_idx = seq_scores.reshape(-1).argsort()
+
         # Sort difficulties
         pixel_proportions_sorted = np.zeros(pixel_proportions.shape)
         pixel_proportions_sorted[sort_idx] = pixel_proportions
-        
+
         # Get pixel values for sequences
         pixel_values = []
         for x in pixel_proportions_sorted:
             pixel_values.append(np.random.normal(loc=x, scale=0.08, size=(10,)))
         pixel_values = np.stack(pixel_values)
 
+        # Loop sequences
+        for sequence_nr in range(pixel_values.shape[0]):
 
+            # Line for sequence start
+            all_the_lines.append(
+                np.array(
+                    [
+                        subject_id + 1,
+                        3,
+                        block_nr + 1,
+                        outcome_factor,
+                        outcome_wiggleroom,
+                        sequence_nr + 1,
+                        pixel_proportions_sorted[sequence_nr],
+                        0,
+                        0,
+                        0,
+                        0,
+                    ]
+                )
+            )
 
+            # Loop trials
+            for trial_nr in range(pixel_values.shape[1]):
 
-        a = np.stack((feedbacks, pixel_proportions_sorted)).T
+                # Line for trials
+                all_the_lines.append(
+                    np.array(
+                        [
+                            subject_id + 1,
+                            5,
+                            block_nr + 1,
+                            outcome_factor,
+                            outcome_wiggleroom,
+                            sequence_nr + 1,
+                            pixel_proportions_sorted[sequence_nr],
+                            0,
+                            0,
+                            trial_nr + 1,
+                            pixel_values[sequence_nr, trial_nr],
+                        ]
+                    )
+                )
 
+            # Line sequence end
+            all_the_lines.append(
+                np.array(
+                    [
+                        subject_id + 1,
+                        4,
+                        block_nr + 1,
+                        outcome_factor,
+                        outcome_wiggleroom,
+                        sequence_nr + 1,
+                        pixel_proportions_sorted[sequence_nr],
+                        feedbacks_non_scaled[sequence_nr, 0],
+                        feedbacks_scaled[sequence_nr, 0],
+                        0,
+                        0,
+                    ]
+                )
+            )
+
+        # Line block end
+        all_the_lines.append(
+            np.array(
+                [
+                    subject_id + 1,
+                    2,
+                    block_nr + 1,
+                    outcome_factor,
+                    outcome_wiggleroom,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                ]
+            )
+        )
+
+        # Plot feedback
+        plt.plot(feedbacks_scaled)
+        plt.hlines(0, 0, 32)
+
+    # Stack lines to array
+    all_the_lines = np.stack(all_the_lines)
+
+    # Save subject file
+    fn = os.path.join(path_out, f"control_file_{str(subject_id+1)}.csv")
+    np.savetxt(fn, all_the_lines, delimiter="\t")
     
-    plt.plot(feedbacks_non_scaled)
-    plt.hlines(0, 0, 32)
-    
+
+    # Columns in file
+    # 01: subject id
+    # 02: event_code (1=blockstart, 2=blockend, 3=sequencestart, 4=sequenceend, 5=trial)
+    # 03: block_nr
+    # 04: block_outcome (-1 = bad, 1 = good)
+    # 05: block_wiggleroom
+    # 06: sequence_nr
+    # 07: sequence_difficulty
+    # 08: sequence_feedback
+    # 09: sequence_feedback_scaled (accumulated)
+    # 10: trial_nr
+    # 11: trial_difficulty
