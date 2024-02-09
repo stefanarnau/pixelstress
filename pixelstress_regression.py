c2dba5f80d8632f2aa8ca9b5ddb758b2c83a4cc0
plkn <fischmechanik@gmail.com>
2024-02-09 17:32:49 +0100

    regression script

--- pixelstress_regression.py
+++ pixelstress_regression.py
@@ -14,6 +14,8 @@ import pandas as pd
 import numpy as np
 import joblib
 import scipy.io
+import sklearn.linear_model
+import sklearn.preprocessing
 
 # Define paths
 path_in = "/mnt/data_dump/pixelstress/2_autocleaned/"
@@ -38,11 +40,29 @@ for dataset in datasets:
     trialinfo.drop(idx_drop, axis=0, inplace=True)
     eeg_data_3d = np.delete(eeg_data_3d, idx_drop, axis=0)
     
+    # Collectors
+    X = []
+    y = []
+    
+    # Iterate sequences n blocks
+    for block in list(set(trialinfo["block_nr"])):
+        for seq in list(set(trialinfo["sequence_nr"])):
+            
+            # Get indices for block-sequence combination
+            idx = list(np.where((trialinfo["block_nr"] == block) & (trialinfo["sequence_nr"] == seq))[0])
+            
+            # Get erp for sequence
+            X.append(eeg_data_3d[idx, :, :].mean(axis=0))
+            
+            #
+            
+
+    
     # Get dims
     n_trial, n_channel, n_time = eeg_data_3d.shape
     
     # Reshape to 2d
-    eeg_data_2d = eeg_data_3d.reshape((n_trial, n_channel * n_time))
+    X = eeg_data_3d.reshape((n_trial, n_channel * n_time))
     
     # Regression predictors:
     # block_nr (to control for time on task and order effects)
@@ -51,16 +71,37 @@ for dataset in datasets:
     # "sequence nr" * "last feedback scaled" (to capture interaction of distance to performance target and deadline imminence)
     
     # Create design matrix
-    design_matrix = trialinfo[["block_nr", "sequence_nr", "last_feedback_scaled"]].to_numpy() 
+    y = trialinfo[["block_nr", "sequence_nr", "last_feedback_scaled"]].to_numpy() 
     
     # Add interaction as product to design matrix
-    design_matrix = np.concatenate((design_matrix, np.multiply(trialinfo[["sequence_nr"]].to_numpy(), trialinfo[["last_feedback_scaled"]].to_numpy())), axis=1)
+    y = np.concatenate((y, np.multiply(trialinfo[["sequence_nr"]].to_numpy(), trialinfo[["last_feedback_scaled"]].to_numpy())), axis=1)
     
+    # A scaler
+    scaler = sklearn.preprocessing.StandardScaler()
+    
+    # Scale data
+    X = scaler.fit_transform(X)
+    y = scaler.fit_transform(y)
+    
+    
+    # A regressor
+    regr = sklearn.linear_model.LinearRegression()
+    
+    # Fit regressor
+    regr.fit(X, y)
+    
+    # Get coefs
+    coef = regr.coef_
+    
+    # Re-reshape coefs to 3d
+    coef_block_nr = coef[0, :].reshape((n_channel, n_time))
+    coef_sequence_nr = coef[1, :].reshape((n_channel, n_time))
+    coef_last_feedback_scaled = coef[2, :].reshape((n_channel, n_time))
+    coef_interaction_feedback_sequence = coef[3, :].reshape((n_channel, n_time))
     
     # TODO: Scaler, Regression, re-reshape, cluster permutation test against zero.
     
-    # Re-reshape to 3d
-    eeg_data_3d = eeg_data_3d.reshape((n_trial, n_channel, n_time))
+
     
     
     aa=bb
