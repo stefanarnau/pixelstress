120163985f72d79985acea198291c83c2207d357
plkn <fischmechanik@gmail.com>
2024-03-13 17:16:00 +0100

    determine experiment lengt

--- pixelstress_first_look.m
+++ pixelstress_first_look.m
@@ -5,7 +5,7 @@ PATH_AUTOCLEANED = '/mnt/data_dump/pixelstress/2_autocleaned/';
 PATH_EEGLAB = '/home/plkn/eeglab2023.1/';
 
 % List of preprocessed datasets
-subject_list = [2, 7, 8, 9, 10, 11 ,12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37];
+subject_list = [2, 7, 8, 9, 10, 11 ,12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38];
 
 % Init eeglab
 addpath(PATH_EEGLAB);
--- pixelstress_prepro.m
+++ pixelstress_prepro.m
@@ -32,7 +32,7 @@ subject_id(to_drop) = [];
 session_condition(to_drop) = [];
 
 % Exclude already done
-already_done = find(ismember(subject_id, [2, 7, 8, 9, 10, 11 ,12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]));
+already_done = find(ismember(subject_id, [2, 7, 8, 9, 10, 11 ,12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]));
 subject_id(already_done) = [];
 session_condition(already_done) = [];
 
